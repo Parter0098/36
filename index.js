ff437037c568b0406411d88f9750341b107f67dc
Saman <samanjan1398@gmail.com>
2024-11-25 17:31:46 +0330

    added

---
+++ index.js
@@ -0,0 +1,11 @@
+var firsName = 'saman'
+var lastName = 'jan jan'
+var age = '19'
+var male = true
+var female = false
+console.log(firsName);
+console.log(lastName);
+console.log(age);
+console.log(male);
+console.log(female);
+
